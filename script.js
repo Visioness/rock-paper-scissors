93bfa50e66cf5783c74ab7be0c518ca469050600
Aygün <130841942+Visioness@users.noreply.github.com>
2025-01-10 15:31:21 +0000

    Create UI for the game

--- script.js
+++ script.js
@@ -1,48 +1,79 @@
+const choices = ["rock", "scissors", "paper"];
+let humanChoice;
 let humanScore = 0;
 let computerScore = 0;
-let choices = ["rock", "paper", "scissors"];
+let round = 1;
+
+const match = document.querySelector("#match");
+const score = document.querySelector("#score");
+const winner = document.querySelector("#winner"); 
+const buttonRock = document.querySelector("#rock");
+const buttonPaper = document.querySelector("#paper");
+const buttonScissors = document.querySelector("#scissors");
+const buttonReplay = document.querySelector("#replay");
+
+const buttonsRPS = [buttonRock, buttonScissors, buttonPaper];
+
+buttonsRPS.forEach(button => {
+  button.addEventListener("click", () => {
+    if (!isGameOver()) {
+      humanChoice = button.id;
+      playRound(humanChoice);
+      updateScoreboard();
+    }
+
+    if (isGameOver()) {
+      buttonRock.disabled = true;
+      buttonPaper.disabled = true;
+      buttonScissors.disabled = true;
+      buttonReplay.style.visibility = "visible";
+      winner.textContent = humanScore === 5 ? "Player Won" : "Computer Won";
+    }
+  });  
+})
+
+buttonReplay.addEventListener("click", () => {
+  resetGame();
+});
 
 function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function getHumanChoice() {
-  let state = prompt("Rock? Paper? Scissors?");
-  if (state === null) return null;
-  return isValid(state.toLowerCase()) ? state.toLowerCase() : getHumanChoice(); 
-}
-
-function isValid(state) {
-  return choices.includes(state);
-}
-
-function playRound(humanChoice, computerChoice) {
-  let states = ["rock", "scissors", "paper"]
-  let humanIndex = states.indexOf(humanChoice);
-  let computerIndex = states.indexOf(computerChoice);
+function playRound(humanChoice) {
+  let computerChoice = getComputerChoice();
+  let humanIndex = choices.indexOf(humanChoice);
+  let computerIndex = choices.indexOf(computerChoice);
+  
+  // Rock Paper Scissors logic
   if (((humanIndex < computerIndex) && (computerIndex - 2 !== humanIndex)) || ((humanIndex > computerIndex) && (humanIndex - 2 === computerIndex))) {
-    console.log(`You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}.`)
+    match.textContent = `You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}.`;
     humanScore++;
   } else if (humanIndex === computerIndex) {
-    console.log("Draw!");
+    match.textContent = "Draw!";
   } else {
-    console.log(`You lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} beats ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}.`)
+    match.textContent = `You lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} beats ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}.`;
     computerScore++;
   }
 }
 
-function playGame() {
-  let computerSelection;
-  let humanSelection;
-  for (let i = 1; i < 6; i++) {
-    computerSelection = getComputerChoice();
-    humanSelection = getHumanChoice();
+function updateScoreboard() {
+  score.textContent = `Player: ${humanScore} - Computer: ${computerScore}`;
+  winner.textContent = `Round ${++round}`;
+}
 
-    if (humanSelection === null) return "Canceled the game!";
-    playRound(humanSelection, computerSelection);
-  }
+function isGameOver() {
+  return humanScore === 5 || computerScore === 5;
+}
 
-  if (humanScore > computerScore) console.log("YOU WON!");
-  else if (humanScore == computerScore) console.log("DRAW!");
-  else console.log("YOU LOST!");
+function resetGame() {
+  humanScore = 0;
+  computerScore = 0;
+  round = 0;
+  buttonRock.disabled = false;
+  buttonPaper.disabled = false;
+  buttonScissors.disabled = false;
+  buttonReplay.style.visibility = "hidden";
+  match.textContent = "Press one of the buttons to start!";
+  updateScoreboard();
 }
